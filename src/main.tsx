280738b231a802d07b4cce1a8dade951cf4872bc
Виталина <130041762+Vitalina06@users.noreply.github.com>
2026-04-22 14:55:23 +0300

    Resolve of the review

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import theme from './theme';
-import { setGlobalAccessToken, setOnUnauthorized } from './api/config';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
